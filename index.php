481fd21732c6ba94724d0c0df80c681826fed129
Tehnikumi Õpilane <opilane@tptlive.ee>
2017-09-13 10:11:14 +0300

    Sügise esimene commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="">
+    <script src="js/app.js"></script>
+  <title>MS-16, 2017 sügis</title>
+</head>
+
+<body>
+    
+    <?php
+        echo "<br> Alanud on 2017 aasta sügis. Siin me nüüd jälle oleme.";
+    
+    ?>
+    
+<script>tere();</script>
+<script>tere1();</script>
+    
+  <img alt="" width="0" height="0" src="">
+    
+</body>
+</html>
+
---
+++ js/app.js
@@ -0,0 +1,17 @@
+var tere ="Tere";
+
+function tere(){
+    
+document.write(tere + " tulemast");
+    
+}
+
+
+(function tere1(){
+    
+var tere ="Tervitus";
+document.write(tere + " tulemast!");
+    
+})
+
+document.write("<br>".tere);
